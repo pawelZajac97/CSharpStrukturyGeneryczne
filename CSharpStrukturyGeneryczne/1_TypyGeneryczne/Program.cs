d3acc5a3cc6b5ce22361cdce3d1cebd07d31dcee
Paweł Zając <pzajc9@gmail.com>
2019-01-19 14:22:16 +0100

    1_4 Kolejka Kołowa

--- CSharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CSharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -10,7 +10,26 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
+            var kolejka = new KolejkaKolowa(3);
 
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine("W naszej kolejce jest : ");
+
+                while (!kolejka.JestPusty)
+               {
+                Console.WriteLine(kolejka.Czytaj());
+               }
         }
     }
 }
