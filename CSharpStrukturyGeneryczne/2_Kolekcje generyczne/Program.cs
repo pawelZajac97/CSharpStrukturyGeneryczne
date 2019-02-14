d9d19f8bba9b0e73063567dbce904fdb9820295a
Paweł Zając <pzajc9@gmail.com>
2019-02-14 21:28:00 +0100

    2_4 Kolejka

--- CSharpStrukturyGeneryczne/2_Kolekcje generyczne/Program.cs
+++ CSharpStrukturyGeneryczne/2_Kolekcje generyczne/Program.cs
@@ -10,22 +10,22 @@ namespace _2_Kolekcje_generyczne
     {
         static void Main(string[] args)
         {
-            var liczby = new List<int>(10); // gdy zostanie przekroczona pojemnosc zostaje podwojona
-            var pojemnosc = -1;
-            while (pojemnosc < 19)
+            Queue<Pracownik> Kolejka = new Queue<Pracownik>(); // FIFO = first in first out;
+            Kolejka.Enqueue(new Pracownik { imie = "Marcin", nazwisko = "Nowak" });
+            Kolejka.Enqueue(new Pracownik { imie = "Tomek", nazwisko = "Nowak" });
+            Kolejka.Enqueue(new Pracownik { imie = "Jacek", nazwisko = "Zajac" });
+            Kolejka.Enqueue(new Pracownik { imie = "Ola", nazwisko = "Kaczor" });
+
+
+            while (Kolejka.Count > 0)
             {
-                if (liczby.Capacity != pojemnosc)
-                {
-                    pojemnosc = liczby.Capacity;
-                    Console.WriteLine(pojemnosc);
-                    pojemnosc++;
-                }
-                liczby.Add(1);
+               var pracownik = Kolejka.Dequeue();
+                Console.WriteLine(pracownik.imie + " " + pracownik.nazwisko);
             }
 
-            
 
-           
+
+
 
 
         }
